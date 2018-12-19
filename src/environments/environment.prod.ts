5096a99142de055d784839c1c611d623a5126dbf
Tijana Gava <tijana.gava@hotmail.com>
2018-12-19 03:23:00 +0100

    Start Client Project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyCizf-AiE_M2kiXMGYnWw99RNo0zz2QSDI",
+        authDomain: "clientpanelprod-e85b3.firebaseapp.com",
+        databaseURL: "https://clientpanelprod-e85b3.firebaseio.com",
+        projectId: "clientpanelprod-e85b3",
+        storageBucket: "clientpanelprod-e85b3.appspot.com",
+        messagingSenderId: "134332480104"
+    }
+};
